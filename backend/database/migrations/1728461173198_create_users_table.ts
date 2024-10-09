e3eba2a7210299ea99574d0a63b6a203758d9afb
0ri0n09 <jojo2209@live.fr>
2024-10-09 16:10:22 +0200

    continue docker & back

--- backend/database/migrations/1728461173198_create_users_table.ts
+++ backend/database/migrations/1728461173198_create_users_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
       table.string('email', 254).notNullable().unique()
-      table.string('password').notNullable()
+      table.string('password')
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
